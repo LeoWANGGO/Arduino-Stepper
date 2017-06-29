536a188e0770fff4d76f512ed481f4b0610fb667
LeoWANGGO <buaawzm@126.com>
2017-06-29 11:14:27 +0800

    Update motor_ctl_test.cpp

--- motor_ctl_test.cpp
+++ motor_ctl_test.cpp
@@ -1,59 +1,77 @@
+//TB6600驱动器
+//采用共阳极接发，脉冲频率设置为600Hz，高低电压电平差4.2V
+//低电平的持续时间为4us，占空比接近0.24%，可见其占空比很小
+//当脉冲频率设置为1000Hz,脉冲宽度9us，占空比0.09%
+//当脉冲频率设置为200Hz,脉冲宽度3.5us，占空比0.07%
+//当脉冲频率设置为100Hz,脉冲宽度3.5us，
 
-int motor1_ena = 2;
-int motor1_dir = 3;
-int motor1_pul = 4;
+const int MOTOR1_ENA = 2;
+const int MOTOR1_DIR = 3;
+const int MOTOR1_PUL = 4;
 
 void setup()
 {
   // put your setup code here, to run once:
-  pinMode(motor1_ena, OUTPUT);
-  pinMode(motor1_dir, OUTPUT);
-  pinMode(motor1_pul, OUTPUT);
+  pinMode(MOTOR1_ENA, OUTPUT);
+  pinMode(MOTOR1_DIR, OUTPUT);
+  pinMode(MOTOR1_PUL, OUTPUT);
 }
 
-//step_deg 转动角度, velocity 转/秒
+//move_motor函数：step_deg 转动角度, velocity 转/秒
 void move_motor( boolean direction, unsigned int step_deg, int velocity)
 {
-  long pulse_r = 200; //每转脉冲数,由硬件设置决定
-  long pulse_number = 0;
-  long pulse_fre = 0;
-  float T_pul = 0;
+  const int PULSE_R = 200; //每转脉冲数,由硬件设置决定
+  float pulse_number;
+  float pulse_fre;
+  float T_pul;
 
-  if ((velocity <= 0)|(step_deg <= 0))
-  {
-    //输入参数错误,中断处理
-  }
-  else
-  {
-    pulse_fre = velocity * pulse_r; //脉冲频率
-    T_pul = 1000 / pulse_fre;  //周期,ms
-    pulse_number = (step_deg / 360) * pulse_r;// 需要走的脉冲数
-  }
+  pulse_fre = PULSE_R * velocity ; //脉冲频率
+  T_pul = 1000 / pulse_fre; //周期,ms
+  pulse_number = (step_deg / 360) * PULSE_R;// 需要走的脉冲数
 
-  //  digitalWrite(motor1_ena, HIGH);
   if (direction == 0)
   {
-    digitalWrite(motor1_dir, HIGH);
+    digitalWrite(MOTOR1_DIR, HIGH);
   }
   else
   {
-    digitalWrite(motor1_dir, LOW);
+    digitalWrite(MOTOR1_DIR, LOW);
   }
-
   while (pulse_number > 0)
   {
-    digitalWrite(motor1_pul, HIGH);
-    delayMicroseconds(T_pul * 0.2);
-    digitalWrite(motor1_pul, LOW);
-    delayMicroseconds(T_pul * 0.8);
+    digitalWrite(MOTOR1_PUL, LOW);
+    delayMicroseconds(T_pul * 0.01);
+    digitalWrite(MOTOR1_PUL, HIGH);
+    delayMicroseconds(T_pul * 0.99);
   }
-  pulse_number --;
+  pulse_number--;
 }
-
+//delayMicroseconds(time);//延迟time微秒
+//示波器实测最终输出会加上指令执行延时2.5us
+//eg：delayMicroseconds(10);实际延时12.5us
 void loop()
 {
   // put your main code here, to run repeatedly:
-  move_motor(0, 90, 1);
-  delay(1000);
-  move_motor(1, 90, 1);
+  /* digitalWrite(MOTOR1_ENA, HIGH);
+    move_motor(0, 1080, 5);
+    digitalWrite(MOTOR1_ENA, LOW);
+    delay(1000);
+
+    digitalWrite(MOTOR1_ENA, HIGH);
+    move_motor(1, 1080, 5);
+    digitalWrite(MOTOR1_ENA, LOW);
+    delay(1000);*/
+    
+  digitalWrite(MOTOR1_DIR, LOW);
+  unsigned int number = 1080; unsigned int fre = 600;
+  long Tm = 1000/fre;
+  long Tu = Tm*1000;
+  while (number > 0)
+  {
+    digitalWrite(MOTOR1_PUL, LOW);
+    delayMicroseconds(Tu*0.005);
+    digitalWrite(MOTOR1_PUL, HIGH);
+    delayMicroseconds(Tu*0.995);
+  }
+  number--;
 }
